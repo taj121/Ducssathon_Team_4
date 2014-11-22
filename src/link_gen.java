9a32582c5790564d9caad56057649ea33c4f8e81
emmet <emmet@emmet-pc>
2014-11-22 15:04:09 +0000

    lolol

--- src/link_gen.java
+++ src/link_gen.java
@@ -1,14 +1,28 @@
 import java.awt.Component;
 import java.net.URL;
+import java.net.URLConnection;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
 import java.awt.Desktop;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.lang.Math;
 
 public class link_gen {
 
 		public static void main(String[] args) throws Exception
 		{
+			String[] urlArray = new String[3];
+			
+			urlArray[0] = ("http://www.cookstr.com/recipes/papaya-and-arugula-salad");
+			urlArray[1] = ("http://www.cookstr.com/recipes/pasta-primavera-2");
+			urlArray[2] = ("http://www.cookstr.com/recipes/french-toast-with-bananas");
+				
+			
 			PointsTrack myPoints = new PointsTrack();
 			JFrame frameStart = new JFrame();
 			Object[] select = { "Yes Please", "No Thanks" };
@@ -35,8 +49,18 @@ public class link_gen {
 				
 				if (userInputSelect2 == 0)
 				{
-					Desktop desktop=Desktop.getDesktop();
-					desktop.browse(new URI("http://www.cookstr.com/searches/surprise"));
+//					Desktop desktop=Desktop.getDesktop();
+//					desktop.browse(new URI("http://www.cookstr.com/searches/surprise"));
+					
+					Random rand = new Random();
+
+					int n = rand.nextInt(2);
+					
+					String url = urlArray[n]; 
+					
+					LinkParser parser = new LinkParser(url);
+					JOptionPane.showMessageDialog(null, "This is a lovely recipe.\n" + parser.parse() + "\nLink to Recipe: " + url);
+					
 				}
 				if (userInputSelect2 == 1)
 				{
@@ -54,6 +78,3 @@ public class link_gen {
 
 		
 }
-
-//Desktop desktop=Desktop.getDesktop();
-//desktop.browse(new URI("http://www.cookstr.com/searches/surprise"));
